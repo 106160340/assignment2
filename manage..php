7465f2906b2acf2e56607da5fac6830b24a57e5f
Erase <yousaff.k.mohammad@gmail.com>
2025-10-22 09:37:42 +1100

    HTML head

---
+++ manage..php
@@ -0,0 +1,12 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="State University EOI" />
+  <meta name="keywords"
+    content="University, jobs, research, digital, computer science, technology, position, IT, application, salary package" />
+  <meta name="HR" content="State University HR team" />
+  <title>Manage</title>
+</head>
