d9f6753e440edc2e5a63ccbf165872d9a1061c6b
Erase <yousaff.k.mohammad@gmail.com>
2025-10-22 09:48:50 +1100

    update head with external css

--- manage..php
+++ manage..php
@@ -3,10 +3,15 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="description" content="State University EOI" />
-  <meta name="keywords"
-    content="University, jobs, research, digital, computer science, technology, position, IT, application, salary package" />
-  <meta name="HR" content="State University HR team" />
-  <title>Manage</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="State University EOI" />
+    <meta name="keywords"
+        content="University, jobs, research, digital, computer science, technology, position, IT, application, salary package" />
+    <meta name="HR" content="State University HR team" />
+    <title>Manage</title>
+    <!-- References to external CSS files-->
+    <link rel="stylesheet" type="text/css" href="styles/style.css" />
+    <!-- References to external responsive CSS file -->
+    <link href="styles/responsive.css" rel="stylesheet" media="screen and (max-width: 3840px)" />
 </head>
+
