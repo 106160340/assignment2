ef506d4d858b28182bd1c5905d7907f870aafef6
Erase <yousaff.k.mohammad@gmail.com>
2025-10-27 20:10:40 +1100

    removed code in head because I added a header.inc

--- manage.php
+++ manage.php
@@ -30,16 +30,8 @@ if (!$dbconn) {
 <?php include 'nav.inc'; ?>
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="State University EOI" />
-    <meta name="keywords"
-        content="University, jobs, research, digital, computer science, technology, position, IT, application, salary package" />
-    <meta name="HR" content="State University HR team" />
-    <title>Manage</title>
-    <!-- References to external CSS files-->
-    <!--<link rel="stylesheet" type="text/css" href="styles/style.css" />-->
-    <!-- References to external responsive CSS file -->
-    <!--<link href="styles/responsive.css" rel="stylesheet" media="screen and (max-width: 3840px)" />-->
+
+
 </head>
 
 <body>
