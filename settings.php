8072f8b457122fac02f6bc2ea759fe5756a779de
106188559 <106188559@student.swin.edu.au>
2025-10-28 15:18:12 +1100

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,27 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "university_db";
-?>
+// settings.php
+$host = "localhost";
+$user = "root";
+$pwd  = "";
+
+// multiple databases
+$db_names = [
+  'jobs' => 'university_db',
+  'about'   => 'about_db',
+  'eoi'  => 'create_eoi.db',
+//   'login' => 'assignment2_db',
+//   'signup' => 'assignment2_db',
+];
+
+function db_connect(string $key = 'default') {
+    global $host, $user, $pwd, $db_names;
+    if (!isset($db_names[$key])) {
+        throw new Exception("Unknown DB key: {$key}");
+    }
+    $conn = mysqli_connect($host, $user, $pwd, $db_names[$key]);
+    if (!$conn) {
+        throw new Exception("DB connect failed: " . mysqli_connect_error());
+    }
+    mysqli_set_charset($conn, 'utf8mb4');
+    return $conn;
+}
